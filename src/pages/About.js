c70a06171da30e084cc1a22623709cdd7bd4c9fd
Salman Khan <salmankhanuet99@gmail.com>
2023-12-01 16:11:32 +0500

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem,
+        assumenda voluptas dolorem deleniti quibusdam cum accusamus aut commodi
+        harum! Rem iusto harum provident placeat maiores consequatur natus
+        libero, vel necessitatibus. Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Corporis repellat, nobis laudantium neque asperiores
+        eligendi est, nihil facilis sint, voluptas dolorum fuga expedita porro
+        culpa! Officiis ipsa rerum praesentium sequi. Lorem, ipsum dolor sit
+        amet consectetur adipisicing elit.
+      </p>
+    </section>
+  );
+};
+
+export default About;
